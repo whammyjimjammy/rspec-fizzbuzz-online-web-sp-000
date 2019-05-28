0f93e319eab34b03504aca150854ad12aec8c283
Jake ORiley <ti1795sv@gmail.com>
2019-05-28 23:31:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  end
+
 
   if int % 5 == 0 # if the number int is divisible by 5
     "Buzz " # Go Buzz
